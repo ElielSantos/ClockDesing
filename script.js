820c5309f88cda9575b2f6e97e8d9c9ef3b89112
ElielSantos <35500688+ElielSantos@users.noreply.github.com>
2023-08-19 20:02:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-
 const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
+const fullscreenButton = document.getElementById('fullscreen-button');
 
-const relogio = setInterval (function time() {
+const relogio = setInterval(function () {
     let dateToday = new Date();
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let s = dateToday.getSeconds();
-    
-    if (hr <10) hr = '0' + hr;
 
-    if (min <10) min = '0' + min;
-
-    if (s <10) s = '0' + s;
-
+    if (hr < 10) hr = '0' + hr;
+    if (min < 10) min = '0' + min;
+    if (s < 10) s = '0' + s;
 
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = s;
+}, 1000);
 
-})
+fullscreenButton.addEventListener('click', toggleFullscreen);
+
+function toggleFullscreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen().catch(err => {
+            console.log(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+        });
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
+}
+
+document.addEventListener('fullscreenchange', () => {
+    if (document.fullscreenElement) {
+        fullscreenButton.textContent = 'Sair da Tela Cheia';
+    } else {
+        fullscreenButton.textContent = 'Tela Cheia';
+    }
+});
